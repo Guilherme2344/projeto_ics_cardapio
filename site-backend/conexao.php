bf8af4707ef5bcb347362aaa1647319d921934b2
Izabel Alice <124362770+IzabelAlice@users.noreply.github.com>
2025-02-05 10:53:28 -0300

    Update conexao.php

--- site-backend/conexao.php
+++ site-backend/conexao.php
@@ -1,4 +1,4 @@
 <?php
-  $conexao = mysqli_connect('192.168.100.20', 'ics', '1fr') or die(mysqli_connect_error($conexao));
-  mysqli_select_db($conexao, 'sitecs') or die(mysqli_error($conexao));
+  $conexao = mysqli_connect('192.168.0.2', 'root', 'ifrn') or die(mysqli_connect_error($conexao));
+  mysqli_select_db($conexao, 'projeto_ics') or die(mysqli_error($conexao));
 ?>
